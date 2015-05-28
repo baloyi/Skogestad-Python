035ea7b7dae52308ef50ade41a185b080e2e868d
baloyi <baloyi@users.noreply.github.com>
2015-05-28 14:32:58 +0200

    Update frequency_response.py
    I have labelled titles and axes and set figure size for the graph to look nice

--- frequency_response.py
+++ frequency_response.py
@@ -7,8 +7,20 @@ def unwrapped_angle(Gfr):
 def bode(w, Gfr):
     plt.subplot(2, 1, 1)
     plt.loglog(w, numpy.abs(Gfr))
+    plt.legend(['power1','power2','power3'])
+    plt.title('Plot of |Gfr| against frequency')
+    plt.xlabel('frequency [rad/s]')
+    plt.ylabel('|Gfr|')
+    
+    
     plt.subplot(2, 1, 2)
     plt.semilogx(w, unwrapped_angle(Gfr))
+    plt.legend(['power1','power2','power3'])
+    plt.title('Plot of Gfr against frequency')
+    plt.xlabel('frequency [rad/s]')
+    plt.ylabel('angle Gfr')
+    
+plt.figure(figsize=(10,12))
 
 w = numpy.logspace(-2, 2)
 s = w * 1j
